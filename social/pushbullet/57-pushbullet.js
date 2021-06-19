e10fd6323f0ad0c635e0e3f132442e3c86597ff1
Stephan Fuchs <sfuchs@fastmail.com>
2021-06-19 20:53:52 +0800

    Version 37

--- social/pushbullet/57-pushbullet.js
+++ social/pushbullet/57-pushbullet.js
@@ -201,8 +201,10 @@ module.exports = function(RED) {
         decipher.update(forge.util.createBuffer(messagePayload));
         decipher.finish();
 
-        var message = decipher.output.toString('utf8');
-        return JSON.parse(message);
+        // var message = decipher.output.toString('utf8');
+        // this.warn('decipher');
+        // this.warn(decipher);
+        // return JSON.parse(message);
     }
 
     PushbulletConfig.prototype.pushMsg = function(incoming) {
